d1871b9f65079af1953dcddcba598bfa47625a51
mhmdnauman <nauman.manutd@gmail.com>
2023-01-09 03:22:23 +0500

    initial commit

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+import React from "react";
+import { Searchbar } from "react-native-paper";
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+
+export const RestaurantsScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Searchbar />
+      </View>
+      <View style={styles.list}>
+        <RestaurantInfoCard />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+  },
+});
+
+export default RestaurantsScreen;
